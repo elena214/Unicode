72464118a2fe224fafa6473922b3aa9183c9a146
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-23 11:06:44 +0100

    Inserimento frase

--- Unicode/Program.cs
+++ Unicode/Program.cs
@@ -10,6 +10,8 @@ namespace Unicode
                 "This unicode string has 2 characters outside the " +
                 "ASCII range: \n" +
                 "Pi (\u03A0), and Sigma (\u03A3).";
+            Console.WriteLine("Inserisci un frase");
+            string fraseInserita = Console.ReadLine();
             Console.WriteLine("Original string:");
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.WriteLine(unicodeString);
